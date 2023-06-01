294266ea84e84d0ba6dab3565433a0b7d3c4122e
LukaD00 <lukadruzijanic00@gmail.com>
2023-06-01 02:30:38 +0200

    save some results

--- infer.py
+++ infer.py
@@ -21,7 +21,7 @@ if __name__=="__main__":
     dataset_docu = DocumentFeaturesDataset.load("features/Docu_val_bert.joblib")
     dataset_para = DocumentFeaturesDataset.load("features/Para_val_bert.joblib")
 
-    classifier_docu = joblib.load("classifiers/Docu_Bert_RF_1800.joblib")
+    classifier_docu = joblib.load("classifiers/Docu_Bert_NN_1.joblib")
     classifier_para = joblib.load("classifiers/Para_Bert_NN_1.joblib")
 
     solutions = {}

9de5dc026e63a04d2296a3d3961824f3042fd041
Luka Druzijanic <luka.druzijanic00@gmail.com>
2023-06-01 16:11:04 +0200

    tmp

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ from features import *
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
 
-
-    
-
-if __name__=="__main__":
+def train_task1():
 
     #features = DocumentBertEmbeddings()
     #dataset = DocumentFeaturesDataset(features)
@@ -20,6 +19,33 @@ if __name__=="__main__":
 
     classifier.fit(dataset.X, dataset.y)
     joblib.dump(classifier, save_file)
+
+
+def train_task2():
+    dataset = ParagraphFeaturesDataset.load("features/Para_train_bert.joblib")
+    classifier = RandomForestClassifier(1800)
+    save_file = "classifiers/Para_Bert_RF_1800"
+
+    classifier.fit(dataset.X, dataset.y)
+    joblib.dump(classifier, save_file)
+    
+
+def train_task3_kmeans():
+    task2_classifier = joblib.load("classifiers/Docu_Bert_NN_1")
+    dataset = ParagraphFeaturesDataset.load("features/Para_train_bert.joblib")
+
+    X = []
+    #for instance in dataset:
+    instance = dataset[0]
+    print(instance.data)
+
+
+
+if __name__=="__main__":
+
+    train_task3_kmeans()
+
+    
 
 
    
